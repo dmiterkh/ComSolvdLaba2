b4bdd0b8b9502b2cfc2eab189fafc8cf26906246
Dmitry Kharevich <dmiterkh@mail.ru>
2021-07-14 02:23:47 +0300

    Sixth Commit

--- src/module-info.java
+++
@@ -1,6 +0,0 @@
-module com.solvd.mobileopeartor {
-	exports com.solvd.mobileoperator.executor;
-	exports com.solvd.mobileoperator.point;
-	exports com.solvd.mobileoperator.staff;
-	exports com.solvd.mobileoperator.user;
-}
